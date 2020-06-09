e0d2739b55d41d1843cfe796c7ef2a9d1df1bfa6
Ebrahim Hazrati <ebrahim.hazrati@topic.nl>
2020-06-09 15:55:13 +0200

    finished chapter_1

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,11 +2,31 @@
 #
 # Exercise 1.27
 
-total_cost = 0
-with open('Data/portfolio.csv') as f:
-    next(f)
-    for line in f:
-        row = line.split(',')
-        total_cost = total_cost + int(row[1]) * float(row[2])
+import csv
+import sys
 
-print(total_cost)
+def portfolio_cost(filename):
+    total_cost = 0
+    f = open(filename)
+    rows = csv.reader(f)
+    headers = next(rows)
+    print(headers)
+    for row in rows:
+        try: 
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost = total_cost + nshares * price
+        except ValueError:
+            print('Bad row: ', row)
+
+        total_cost = total_cost + nshares * price
+    f.close()
+    return total_cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else: 
+    filename = input('Enter a file name:')
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
