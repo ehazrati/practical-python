dfe390c9f2493afa5992e20011a4e80e6ed831dc
Ebrahim Hazrati <ebrahim.hazrati@topic.nl>
2020-06-15 14:06:09 +0200

    up to 3.3

--- Work/report.py
+++ Work/report.py
@@ -1,94 +1,80 @@
 # report.py
-#
-# Exercise 2.4
-
 import csv
-import sys
 
 def read_portfolio(filename):
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
     portfolio = []
-
-    with open(filename, 'rt') as f:
+    with open(filename) as f:
         rows = csv.reader(f)
         headers = next(rows)
-        for rowno, row in enumerate(rows, start=1):
+
+        for row in rows:
             record = dict(zip(headers, row))
-            name = record['name']
-            nshares = int(record['shares'])
-            price = float(record['price'])
-            portfolio.append({'name': name, 'shares': nshares, 'price': price})
+            stock = {
+                'name' : record['name'],
+                'shares' : int(record['shares']),
+                'price' : float(record['price'])
+            }
+            portfolio.append(stock)
 
     return portfolio
 
 def read_prices(filename):
+    '''
+    Read a CSV file of price data into a dict mapping names to prices.
+    '''
     prices = {}
-    
-    with open(filename, 'rt') as f:
+    with open(filename) as f:
         rows = csv.reader(f)
-        headers = ['name', 'price']
         for row in rows:
-            if len(row) == 0:
-                continue 
-
-            record = dict(zip(headers, row))
             try:
-               # record = dict(zip(headers, row))
-                name = record['name']
-                price = float(record['price'])
-                prices[name] = price
+                prices[row[0]] = float(row[1])
             except IndexError:
-                print('Bad row: ', row)
+                pass
+
     return prices
 
-
-def make_report(portfolio, prices):
-    report = []
+def make_report_data(portfolio,prices):
+    '''
+    Make a list of (name, shares, price, change) tuples given a portfolio list
+    and prices dictionary.
+    '''
+    rows = []
     for stock in portfolio:
-        name = stock['name']
-        nshares = stock['shares']
-        price = stock['price']
-        current_price = prices[name]
-        change = current_price - price
-        report.append((name, nshares, price, change))
-    return report
+        current_price = prices[stock['name']]
+        change = current_price - stock['price']
+        summary = (stock['name'], stock['shares'], current_price, change)
+        rows.append(summary)
+    return rows
 
-
-def print_report(report):
-    headers = ('Name', 'Shares', 'Price', 'Change')
+def print_report(reportdata):
+    '''
+    Print a nicely formated table from a list of (name, shares, price, change) tuples.
+    '''
+    headers = ('Name','Shares','Price','Change')
     print('%10s %10s %10s %10s' % headers)
-    print(('-' * 10 + ' ') * len(headers))
-    for row in report:
+    print(('-'*10 + ' ')*len(headers))
+    for row in reportdata:
         print('%10s %10d %10.2f %10.2f' % row)
 
+def portfolio_report(portfoliofile,pricefile):        
+    '''
+    Make a stock report given portfolio and price data files.
+    '''
+    # Read data files 
+    portfolio = read_portfolio(portfoliofile)
+    prices = read_prices(pricefile)
 
+    # Create the report data
+    report = make_report_data(portfolio,prices)
 
-#if len(sys.argv) == 3:
-#    portfolio_filename = sys.argv[1]
-#    prices_filename = sys.argv[2]
-#else:
-#    portfolio_filename = input('Enter portfolio filename: ')
-#    prices_filename = input('Enter prices filename: ')
-
-portfolio_filename = 'Data/portfolio.csv' 
-prices_filename = 'Data/prices.csv'
-
-
-
-#portfolio = read_portfolio(portfolio_filename)
-#prices = read_prices(prices_filename)
-
-#report = make_report(portfolio, prices)
-#print_report(report)
-
-def portfolio_report(portfolio_filename, prices_filename):
-    portfolio = read_portfolio(portfolio_filename)
-    prices = read_prices(prices_filename)
-
-    report = make_report(portfolio, prices)
+    # Print it out
     print_report(report)
 
-portfolio_report(portfolio_filename, prices_filename)
-
-
+portfolio_report('Data/portfolio.csv',
+                 'Data/prices.csv')
 
 
